2a8ea8692db15267a6f1b382df5ec32f485895f0
Joao G.A. Amorim <joaogustavoamorim@gmail.com>
2022-01-22 19:29:51 -0300

    try fix the conf to generate the docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("example_module/"))
+sys.path.insert(0, os.path.abspath("../example_module/"))
 
 
 # -- Project information -----------------------------------------------------
